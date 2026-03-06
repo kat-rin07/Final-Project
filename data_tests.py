c6312a6dabcc907ed44faef723c21550ac7f34e9
kat-rin07 <kathringutierrez1@gmail.com>
2026-03-05 21:33:43 -0800

    Classes Student and HealthRecord w/ Test cases for student + healthrecord

--- data_tests.py
+++ data_tests.py
@@ -1,7 +1,10 @@
 import data
 import unittest
 
-#Test for Classes started by Kathrin (mark done when checked/or fixed)
+from data import HealthRecord
+
+
+#Test for Classes Written by Kathrin (mark done when checked/or fixed)
 #Test for Classes debugged/fixed by ___
 
 class DataTests(unittest.TestCase):
@@ -45,5 +48,43 @@ class DataTests(unittest.TestCase):
     def test_Student_repr(self):
         student = data.Student('Participant 1', 'Freshman', 'Santa Lucia')
         self.assertEqual(repr(student),
-                         "Student('Participant 1', 'Freshman', 'Santa Lucia')"
-)
+                         "Student('Participant 1', 'Freshman', 'Santa Lucia')")
+
+#Test 1 - Class HealthRecord
+
+    def test_HealthRecord(self):
+        record = data.HealthRecord('Participant 1', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        self.assertEqual(record.id, 'Participant 1')
+        self.assertEqual(record.year, 'Freshman')
+        self.assertEqual(record.dorm, 'Santa Lucia')
+        self.assertEqual(record.illness, 'Common Cold')
+        self.assertEqual(record.frequency, '3')
+
+#Test 2 - Class HealthRecord
+    def test_HealthRecord_eq_1(self):
+        student_1 = data.HealthRecord('Participant 1', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        student_2 = data.HealthRecord('Participant 1', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        self.assertEqual(student_1, student_2)
+
+#Test 3 - Class HealthRecord
+
+    def test_HealthRecord_eq_2(self):
+        student = data.HealthRecord('Participant 1', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        self.assertEqual(student, student)
+
+#Test 4 - Class Health Record
+    def test_HealthRecord_eq_3(self):
+        student = data.HealthRecord('Participant 1', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        student1 = data.HealthRecord('Participant 2', 'Transfer', "Poly Canyon Village", 'Flu', '1')
+        self.assertNotEqual(student, student1)
+
+#Test 5 - Class Health Record
+    def test_HealthRecord_eq_4(self):
+        student = data.HealthRecord('Participant', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        other = 10
+        self.assertNotEqual(student, other)
+
+    def test_HealthRecord_repr(self):
+        student = data.HealthRecord('Participant', 'Freshman', 'Santa Lucia', 'Common Cold', '3')
+        self.assertEqual(repr(student),
+                         "HealthRecord('Participant', 'Freshman', 'Santa Lucia', 'Common Cold', '3')")
