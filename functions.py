c63ee3cf1e2441d6b0d15d19f234d925d711688e
mgull <mgullick@calpoly.edu>
2026-03-06 23:33:11 -0800

    Mar Push

--- functions.py
+++ functions.py
@@ -7,7 +7,7 @@ import data
 
 #Functions go here!
 #Marissa's Functions
-def cases(records):
+def illness_cases(records):
     count = {}
     for record in records:
         illness = record.illness
@@ -17,7 +17,7 @@ def cases(records):
             count[illness] = 1
     return count
 
-def most_cases(count):
+def illness_with_most_cases(count):
     top_illness = []
     top_illness_count = 0
     for illness in count:
@@ -28,3 +28,25 @@ def most_cases(count):
             top_illness.append(illness)
 
     return top_illness, top_illness_count
+
+def dorms(records):
+    dorm_count = {}
+    for record in records:
+        dorm = record.dorm
+        if dorm in dorm_count:
+            dorm_count[dorm] += 1
+        else:
+            dorm_count[dorm] = 1
+    return dorm_count
+
+def dorm_with_most_cases(dorm_count):
+    top_dorm = []
+    top_dorm_count = 0
+    for dorm in dorm_count:
+        if dorm_count[dorm] > top_dorm_count:
+            top_dorm_count = dorm_count[dorm]
+            top_dorm = [dorm]
+        elif dorm_count[dorm] ==  top_dorm_count:
+            top_dorm.append(dorm)
+
+    return top_dorm, top_dorm_count
